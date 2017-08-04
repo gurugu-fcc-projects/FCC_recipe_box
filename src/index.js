2c802a554eb5f94154698de35ef2f4129651f733
Peter Krevenets <peter.k.nets@gmail.com>
2017-08-04 19:39:11 +0300

    Create react-redux folder structure & add Roboto font & react-tap dependency

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './styles/index.css';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import registerServiceWorker from './registerServiceWorker';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+injectTapEventPlugin();
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
