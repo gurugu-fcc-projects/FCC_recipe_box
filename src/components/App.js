122f20b0e079d712cf51f08e4f5926b325360908
Peter Krevenets <peter.k.nets@gmail.com>
2017-08-05 12:30:04 +0300

    Add App Bar with app name

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
+import AppBar from 'material-ui/AppBar';
 
 import '../styles/App.css';
-import logo from '../logo.svg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <RaisedButton label="Default" />
+        <AppBar
+          title="Recipe Box"
+          showMenuIconButton={false}
+          style={{backgroundColor: '#F06292'}}
+        />
       </div>
     );
   }
