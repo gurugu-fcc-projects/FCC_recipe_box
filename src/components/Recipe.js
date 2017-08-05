26d49d49b788a2e7e02f5a48db654115bc4204d3
Peter Krevenets <peter.k.nets@gmail.com>
2017-08-05 16:57:13 +0300

    Add Edit and Delete buttons & style ingredients some more

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -4,7 +4,10 @@ import {
   Card,
   CardHeader,
   CardText,
+  CardTitle,
 } from 'material-ui/Card';
+import Subheader from 'material-ui/Subheader';
+import Divider from 'material-ui/Divider';
 import {
   Table,
   TableBody,
@@ -13,6 +16,22 @@ import {
   TableRow,
   TableRowColumn,
 } from 'material-ui/Table';
+import RaisedButton from 'material-ui/RaisedButton';
+
+
+const styles = {
+  subheader: {
+    lineHeight: '20px',
+    marginBottom: '5px',
+  },
+  tableRow: {
+    padding: '0',
+    margin: '0',
+  },
+  raisedButton: {
+    margin: '10px 10px 0 0',
+  },
+};
 
 const Recipe = ({ recipe }) => (
   <Card>
@@ -21,15 +40,24 @@ const Recipe = ({ recipe }) => (
       actAsExpander={true}
       showExpandableButton={true} />
     <CardText expandable={true}>
-      <Table>
-        <TableBody>
+      <Subheader style={styles.subheader}>Ingredients</Subheader>
+      <Divider />
+      <Table multiSelectable={true}>
+        <TableBody displayRowCheckbox={true}>
           {recipe.ingredients.map((ingredient, index) =>
-            <TableRow key={index}>
+            <TableRow key={index} displayBorder={false} style={styles.tableRow}>
               <TableRowColumn>{ingredient}</TableRowColumn>
             </TableRow>
           )}
         </TableBody>
       </Table>
+      {/* {recipe.ingredients.map((ingredient, index) =>
+        <Card key={index}>
+          <CardHeader title={ingredient} />
+        </Card>
+      )} */}
+      <RaisedButton label="Edit" style={styles.raisedButton} />
+      <RaisedButton label="Delete" secondary={true} style={styles.raisedButton} />
     </CardText>
   </Card>
 );
